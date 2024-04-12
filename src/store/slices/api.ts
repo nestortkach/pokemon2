433dbb243326c5e236fc80040c4b9b47d3d223a0
Volodymyr Zhyhaylo <appexoft@Volodymyrs-Mac-mini.local>
2024-04-12 14:23:15 +0300

    feat: done typization

--- src/store/slices/api.ts
+++ src/store/slices/api.ts
@@ -1,12 +1,26 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import {useHttp} from '../../hooks/http.hook';
 
+interface Pokemon {
+    name: string;
+    thumbnail: string;
+    moves: string[];
+    // eslint-disable-next-line
+    types: any[];
+}
+
+interface TypedPokemons {
+    // eslint-disable-next-line
+    pokemons: any[];
+}
+
 const _singleBaseUrl = 'https://pokeapi.co/api/v2/pokemon/';
 const _typesBaseUrl = 'https://pokeapi.co/api/v2/';
 
 export const fetchPokemons = createAsyncThunk(
     'pokemons/fetchPokemons',
-    async (url: string) => {
+    // eslint-disable-next-line
+    async (url: string): Promise<{response: any, url: string}> => {
         const {request} = useHttp();
         const response = await request(url);
         return {response, url};
@@ -15,17 +29,17 @@ export const fetchPokemons = createAsyncThunk(
 
 export const fetchSinglePokemon = createAsyncThunk(
     'pokemons/fetchSinglePokemon',
-    async (pokemon: string | undefined) => {
+    async (pokemon: string | undefined): Promise<Pokemon> => {
         const {request} = useHttp();
         const response = await request(`${_singleBaseUrl + pokemon}`);
-        // console.log(response)
         return _transformPokemon(response);
     }    
 )
 
 export const fetchTypes = createAsyncThunk(
     'pokemons/fetchTypes',
-    async () => {
+    // eslint-disable-next-line
+    async (): Promise<{response: any}> => {
         const {request} = useHttp();
         const response = await request(`${_typesBaseUrl + 'type'}`);
         return {response};
@@ -34,24 +48,26 @@ export const fetchTypes = createAsyncThunk(
 
 export const fetchTypedPokemons = createAsyncThunk(
     'pokemons/fetchTypedPokemon',
-    async (type: string) => {
+    async (type: string): Promise<TypedPokemons> => {
         const {request} = useHttp();
         const response = await request(`${_typesBaseUrl + 'type/' + type}`);
         return _transformTypedPokemons(response);
     }    
 )
-
-const _transformPokemon = (pokemon: any) => {
+// eslint-disable-next-line
+const _transformPokemon = (pokemon: any): Pokemon => {
     return {
         name: pokemon.name,
         thumbnail: pokemon.sprites.front_default,
+        // eslint-disable-next-line
         moves: pokemon.moves.map((item: any) => item.move.name),
         types: pokemon.types
     }
 }
-
-const _transformTypedPokemons = (pokemon: any) => {
+// eslint-disable-next-line
+const _transformTypedPokemons = (pokemon: any): TypedPokemons => {
     return {
+        // eslint-disable-next-line
         pokemons: pokemon.pokemon.map((item: any) => item.pokemon),
     }
 }
--- src/store/slices/pokemonListSlice.ts
+++ src/store/slices/pokemonListSlice.ts
@@ -1,11 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchPokemons, fetchSinglePokemon, fetchTypedPokemons} from "./api";
 
+interface Pokemon {
+    name: string;
+    thumbnail: string;
+    moves: string[];
+    // eslint-disable-next-line
+    types: any[];
+}
+
+interface TypedPokemons {
+    // eslint-disable-next-line
+    pokemons: any[];
+}
+
+type LoadingStatus = 'idle' | 'loading' | 'error';
+
 const initialState = {
-    pokemonLoadingStatus: "idle",
-    pokemonsList: [],
+    pokemonLoadingStatus: "idle" as LoadingStatus,
+    pokemonsList: [] as TypedPokemons['pokemons'],
     pagination: {next: '', prev: '', curr: 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=20'},
-    pokemonInfo: {name: null, thumbnail: '', moves: [],  types: []},
+    pokemonInfo: {name: '', thumbnail: '', moves: [],  types: []} as Pokemon,
 };
 
 const pokemonsListSlice = createSlice({
